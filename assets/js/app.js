11fcb10917c58b099adbe888709393d267c1e5bd
Yezir971 <james_ahmedaly@yahoo.com>
2024-01-22 16:34:27 +0100

    code input

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,19 @@
 document.addEventListener('DOMContentLoaded', function(){
     let button = document.getElementsByTagName('button');
     let screen = document.getElementById('mdp');
-    
+    let code = "2589";
     console.log(button)
     let size = button.length;
-    for(let i=0; i<size ; i++){
-        button[i].addEventListener('click', ()=>{
-            screen.value += button[i].value
-            console.dir(button[i].value);
-
-        })
+    
+    let j =0;
+    if(j<3){
+        for(let i=0; i<size ; i++){
+            button[i].addEventListener('click', ()=>{
+                screen.value += button[i].value
+                console.dir(button[i].value);
+            })
+        }
+        j++;
     }
+    console.log('gg')
 });

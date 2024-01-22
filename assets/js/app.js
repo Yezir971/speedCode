0ad3d108ef525b08dbabfed8191f5dac4db77189
Yezir971 <james_ahmedaly@yahoo.com>
2024-01-22 16:18:51 +0100

    button interactive

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,15 @@
 
 document.addEventListener('DOMContentLoaded', function(){
     let button = document.getElementsByTagName('button');
+    let screen = document.getElementById('mdp');
+    
+    console.log(button)
     let size = button.length;
-    for(let i; i<size ; i++){
-        // button[i]
+    for(let i=0; i<size ; i++){
+        button[i].addEventListener('click', ()=>{
+            screen.value += button[i].value
+            console.dir(button[i].value);
+
+        })
     }
-    console.log('gg');
 });
